cc693cd87fc0be6ff31929e8e3a5381b48fcfb93
jerem <jeremydeproote@hotmail.com>
2023-03-29 09:18:14 +0200

    nouvelle version 1

--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 from tkinter import *
-
-import ttkbootstrap
+from tkinter import messagebox
+from ttkbootstrap.icons import Emoji
+from ttkbootstrap.scrolled import ScrolledText
 import ttkbootstrap as ttk
 import time
 
@@ -38,8 +39,54 @@ class Sheetcheat(ttk.Toplevel):
         self.title("SheetCheat")
         self.iconbitmap("Logo.ico")
         self.geometry("800x400")
-        self.qbutt = ttk.Button(self, command=self.exit_program)
-        self.qbutt.pack()
+        self.topbar = ttk.Frame(self)
+        self.topbar.columnconfigure(4, weight=1)
+        self.pack_propagate(False)
+
+        # Creation of the button NEW and insertion of the image in the button
+        self.my_new = ttk.PhotoImage(file="images/mail.png")
+        self.new = ttk.Button(self.topbar, image=self.my_new)
+        self.new.grid(column=0, row=0, pady=5, padx=5, sticky=ttk.W)
+
+        # Creation of the button SAVE and insertion of the image in the button
+        self.my_save = ttk.PhotoImage(file="images/map.png")
+        self.save = ttk.Button(self.topbar, image=self.my_save)
+        self.save.grid(column=1, row=0, pady=5, padx=5, sticky=ttk.W)
+
+        # Creation of the button SAVE AS and insertion of the image in the button
+        self.my_save_as = ttk.PhotoImage(file="images/minus.png")
+        self.save_as = ttk.Button(self.topbar, image=self.my_save_as, command="")
+        self.save_as.grid(column=2, row=0, pady=5, padx=5, sticky=ttk.W)
+
+        # Creation of the button OPEN and insertion of the image in the button
+        self.my_open = ttk.PhotoImage(file="images/plus.png")
+        self.open = ttk.Button(self.topbar, image=self.my_open, command="")
+        self.open.grid(column=3, row=0, pady=5, padx=5, sticky=ttk.W)
+
+        # Creation of the button AP and insertion of the image in the button
+        self.my_ap = ttk.PhotoImage(file="images/map.png")
+        self.ap = ttk.Button(self.topbar, image=self.my_ap, command="")
+        self.ap.grid(column=4, row=0, pady=5, padx=5, sticky=ttk.E)
+
+        # Creation of the button EXIT and insertion of the image in the button
+        self.my_exit = ttk.PhotoImage(file="images/plus.png")
+        self.exit = ttk.Button(self.topbar, image=self.my_exit, command=self.exitapp)
+        self.exit.grid(column=5, row=0, pady=5, padx=5, sticky=ttk.E)
+
+        self.texte = ScrolledText(self, padding=5, autohide=True, hbar=True)
+
+        self.topbar.pack(expand=False, fill=ttk.X, side=ttk.TOP, anchor=ttk.N)
+        self.texte.pack(expand=True, fill=ttk.BOTH)
+
+    def exitapp(self) -> None:
+        MsgBox = messagebox.askquestion(
+            "Exit", "Voulez vous quitter l'application?", icon="info"
+        )
+        if MsgBox == "yes":
+            messagebox.showinfo("A bientôt", "Merci d'avoir utiliser l'application")
+            self.quit()
+        else:
+            messagebox.showinfo("Good", "Merci " + Emoji.get("winking face").char * 3)
 
     def exit_program(self):
         exit(0)
