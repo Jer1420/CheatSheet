60a79e777c354995d299f0fee2b10a40fb6a4731
jerem <jeremydeproote@hotmail.com>
2023-03-29 15:06:29 +0200

    correction avec le prof

--- view.py
+++ view.py
@@ -1,41 +1,15 @@
-from tkinter import *
-from tkinter import messagebox
-from ttkbootstrap.icons import Emoji
-from ttkbootstrap.scrolled import ScrolledText
-import ttkbootstrap as ttk
 import time
+from tkinter import PhotoImage, messagebox
+from ttkbootstrap.scrolled import ScrolledText
+
+import ttkbootstrap as ttk
+from ttkbootstrap.icons import Emoji
 
 
-class ProgressBar(ttk.Window):
+class Test(ttk.Window):
     def __init__(self):
         super().__init__(themename="solar")
-        self.title("Chargement")
-        self.geometry("521x397")
-        self.iconbitmap("Logo.ico")
-
-        self.my_fond = PhotoImage(file="Logo_tfe.png")
-        self.my_background = ttk.Label(self, image=self.my_fond)
-        self.my_background.place(x=0, y=0, relwidth=1, relheight=1)
-        self.my_progress = ttk.Progressbar(self, style="warning-striped", maximum=100, mode="determinate", length=300, value=0)
-        self.my_progress.pack(pady=190)
-
-    def progression(self):
-        for i in range(100):
-            self.my_progress["value"] = i
-            time.sleep(0.02)
-            self.my_progress.update()
-            self.update_idletasks()
-        else:
-            self.withdraw()
-            sheet_cheat_window = Sheetcheat()
-
-    def start_progress_bar(self):
-        self.mainloop()
-
-
-class Sheetcheat(ttk.Toplevel):
-    def __init__(self):
-        super().__init__()
+        self.withdraw()
         self.title("SheetCheat")
         self.iconbitmap("Logo.ico")
         self.geometry("800x400")
@@ -88,12 +62,39 @@ class Sheetcheat(ttk.Toplevel):
         else:
             messagebox.showinfo("Good", "Merci " + Emoji.get("winking face").char * 3)
 
-    def exit_program(self):
-        exit(0)
+
+class TopTest(ttk.Toplevel):
+    def __init__(self,app):
+        super().__init__()
+        self.title("Chargement")
+        self.geometry("521x397")
+        self.iconbitmap("Logo.ico")
+        self.app = app
+
+        self.my_fond = PhotoImage(file="Logo_tfe.png")
+        self.my_background = ttk.Label(self, image=self.my_fond)
+        self.my_background.place(x=0, y=0, relwidth=1, relheight=1)
+        self.my_progress = ttk.Progressbar(self, style="warning-striped", maximum=100, mode="determinate", length=300, value=0)
+        self.my_progress.pack(pady=190)
+
+        self.progression()
+
+    def progression(self):
+        for i in range(100):
+            self.my_progress["value"] = i
+            time.sleep(0.02)
+            self.my_progress.update()
+            self.update_idletasks()
+        else:
+            self.app.deiconify()
+            self.destroy()
+
+    def done(self):
+        self.destroy()
+        self.app.deiconify()
 
 
 if __name__ == '__main__':
-    app = ProgressBar()
-    app.progression()
-    app.start_progress_bar()
-
+    test = Test()
+    toptest = TopTest(test)
+    toptest.mainloop()
